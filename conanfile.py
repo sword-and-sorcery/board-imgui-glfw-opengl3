936ea8d653d9daf283f3ea1e599c9253b1860c0a
Javier G. Sogo <jgsogo@gmail.com>
2019-07-23 16:50:39 +0200

    simplify files

--- conanfile.py
+++ conanfile.py
@@ -23,15 +23,10 @@ class BoardImgui(ConanFile):
         self.requires("ui-board_game/0.0@sword/sorcery")
         self.requires("ui-tileset-glfw/0.0@sword/sorcery")
         self.requires("boost/1.69.0@conan/stable")
-        # self.requires("glfw/3.2.1@bincrafters/stable")
-        # self.requires("rapidxml/1.13@bincrafters/stable")
-        # self.requires("boost_filesystem/1.69.0@bincrafters/stable")
-        # self.requires("stb/20180214@conan/stable")
 
     def imports(self):
-        bindings_dir = os.path.join(self.source_folder, "bindings")
-        self.copy("imgui_impl_glfw*", src="misc/bindings", dst=bindings_dir, root_package="imgui")
-        self.copy("imgui_impl_opengl3*", src="misc/bindings", dst=bindings_dir, root_package="imgui")
+        self.copy("imgui_impl_glfw*", src="misc/bindings", dst="bindings", root_package="imgui")
+        self.copy("imgui_impl_opengl3*", src="misc/bindings", dst="bindings", root_package="imgui")
 
     def build(self):
         cmake = CMake(self)
